e037dd15768e7b2882a359048d9b8f5ee783456b
sreshetnyak <reshetniak.sergey@gmail.com>
2014-02-08 20:30:05 +0200

    Initial commit from local git

---
+++ UIViewGeometry/TTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTViewController.h
+//  UIViewGeometry
+//
+//  Created by sergey on 2/8/14.
+//  Copyright (c) 2014 sergey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTViewController : UIViewController
+
+@end
